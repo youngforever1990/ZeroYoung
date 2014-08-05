093b440fcc9fe794f9ad2086f0ebed5af722e06d
youngforever1990 <highyoung1990@gmail.com>
2014-08-05 16:23:13 +0800

    test git commit using terminal

---
+++ HelloWorld002/HelloWorld002/ZOYAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZOYAppDelegate.h
+//  HelloWorld002
+//
+//  Created by victor on 14-8-5.
+//  Copyright (c) 2014年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZOYAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ HelloWorld002/HelloWorld002/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  HelloWorld002
+//
+//  Created by victor on 14-8-5.
+//  Copyright (c) 2014年 Young. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZOYAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZOYAppDelegate class]));
+    }
+}
